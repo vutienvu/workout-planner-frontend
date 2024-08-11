2774c4175a7b53b79c46b29f1313f952c764f3ad
Tien Vuong Vu <vutienvu@cvut.cz>
2024-08-11 19:55:28 +0200

    [feat]: Add router

--- src/app/components/Sidebar.vue
+++ src/app/components/Sidebar.vue
@@ -6,7 +6,7 @@
           <v-skeleton-loader v-if="isFetchingWorkouts" type="list-item-two-line"></v-skeleton-loader>
 
           <div v-else v-if="workouts.length > 0" v-for="workout in workouts" :key="workout.workoutId" class="position-relative">
-              <v-list-item link height=50 :title="workout.name" @click="handleLinkClick"></v-list-item>
+              <v-list-item link height=50 :title="workout.name" @click="handleOpenWorkout"></v-list-item>
               <div class="position-absolute right-0" style="top: 50%; transform: translateY(-50%)">
                 <v-btn icon="mdi-pencil" size="x-small"  color="primary" @click="handleRenameWorkout" class="mr-1"></v-btn>
                 <v-btn icon="mdi-delete" size="x-small" @click="handleDeleteWorkout" class="mr-1"></v-btn>
@@ -18,10 +18,13 @@
 
 <script setup lang="ts">
 import {onMounted, ref} from 'vue'
+import {useRouter} from 'vue-router'
 import {getWorkouts, Workout} from '../api/WorkoutAPI.ts'
 
 const workouts = ref<Workout[]>([]);
 const isFetchingWorkouts = ref<boolean>(true);
+
+const router = useRouter();
 
 onMounted(async () => {
   getWorkouts()
@@ -36,8 +39,8 @@ onMounted(async () => {
           });
 });
 
-const handleLinkClick = () => {
-  alert("Move to /workout/workoutId");
+const handleOpenWorkout = () => {
+  router.push('/workout');
 }
 
 const handleRenameWorkout = (event: Event) => {
