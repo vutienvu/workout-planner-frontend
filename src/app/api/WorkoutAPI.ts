42578800437d9c20aacd99dec87b87f91cbc6c89
Tien Vuong Vu <vutienvu@cvut.cz>
2024-08-16 19:13:07 +0200

    [feat]: Implement create workout main logic

--- src/app/api/WorkoutAPI.ts
+++ src/app/api/WorkoutAPI.ts
@@ -1,6 +1,10 @@
 import axiosInstance from './BaseAPI.ts'
 import { Exercise } from './ExerciseAPI.ts'
 
+export interface WorkoutRequest {
+    name: string
+}
+
 export interface Workout {
     workoutId: number
     name: string
@@ -29,6 +33,16 @@ export async function getWorkout(workoutId: number): Promise<Workout> {
 
 export async function deleteWorkout(workoutId: number) {
     return axiosInstance.delete<Workout>(`/workouts/${workoutId}`)
+        .catch(error => {
+            throw error;
+        });
+}
+
+export async function createWorkout(newWorkout: WorkoutRequest) {
+    return axiosInstance.post<WorkoutRequest>(`/workouts`, newWorkout)
+        .then(response => {
+            return response.data;
+        })
         .catch(error => {
             throw error;
         });
--- src/app/components/CreateWorkoutModal.vue
+++ src/app/components/CreateWorkoutModal.vue
@@ -9,7 +9,7 @@
         prepend-icon="mdi-pencil"
         title="Create your new workout!"
     >
-      <v-text-field label="Workout name" variant="underlined" class="px-6"></v-text-field>
+      <v-text-field v-model="workoutName" label="Workout name" variant="underlined" :rules="[workoutNameRules.required, workoutNameRules.noWhiteSpaces]" class="px-6"></v-text-field>
       <template v-slot:actions>
         <v-btn
             class="ms-auto"
@@ -29,9 +29,28 @@
 
 <script setup lang="ts">
 import {ref} from 'vue'
+import {computed, ref} from 'vue'
+import {createWorkout, WorkoutRequest} from '../api/WorkoutAPI.ts'
 
 
 const isOpen = ref<boolean>(false);
+const workoutName = ref<string>("");
+
+const handleCreateWorkout = () => {
+  const newWorkout: WorkoutRequest = {
+    name: workoutName.value
+  };
+
+  createWorkout(newWorkout)
+      .then(workout => {
+        console.log(workout)
+      })
+      .catch(error => {
+        console.log(error);
+      })
+
+}
+
 </script>
 
 <style scoped>
