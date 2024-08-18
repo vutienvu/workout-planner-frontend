58b74fc538f05210cf8848b5686a445c6d700250
Tien Vuong Vu <vutienvu@cvut.cz>
2024-08-18 18:27:05 +0200

    [feat]: Setup edit/remove exercise

--- src/app/components/Exercise.vue
+++ src/app/components/Exercise.vue
@@ -1,13 +1,13 @@
 <template>
-  <v-card :title="props.name" hover class="mt-8" elevation="2">
+  <v-card :title="props.name" hover class="mt-8" elevation="2" @click="handleOpen">
     <template v-slot:subtitle>
       <v-icon icon="mdi-timer" size="20"></v-icon>
       {{ props.pauseDuration + "s" }}
     </template>
 
     <template v-slot:append>
-      <v-btn icon="mdi-pencil" color="primary" size="small" class="mr-2"></v-btn>
-      <v-btn icon="mdi-delete" size="small"></v-btn>
+      <v-btn icon="mdi-pencil" color="primary" size="small" class="mr-2" @click="(e: Event) => handleEdit(e)"></v-btn>
+      <v-btn icon="mdi-delete" size="small" @click="(e: Event) => handleRemove(e)"></v-btn>
     </template>
   </v-card>
 </template>
@@ -20,6 +20,20 @@ interface Props {
 }
 
 const props = defineProps<Props>();
+
+const handleOpen = () => {
+  console.log("Open exercise!");
+}
+
+const handleEdit = (e: Event) => {
+  e.stopPropagation();
+  console.log("Edit exercise!");
+}
+
+const handleRemove = (e: Event) => {
+  e.stopPropagation();
+  console.log("Remove exercise!");
+}
 
 
 </script>
