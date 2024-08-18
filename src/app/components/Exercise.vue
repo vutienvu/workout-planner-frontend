c4177999e306ab52dd199bce7c02247289abf773
Tien Vuong Vu <vutienvu@cvut.cz>
2024-08-18 18:23:18 +0200

    [feat]: Update exercise card

--- src/app/components/Exercise.vue
+++ src/app/components/Exercise.vue
@@ -1,5 +1,15 @@
 <template>
-  <v-card :title="props.name" :subtitle="props.pauseDuration + 's'" hover class="mt-8" elevation="2"></v-card>
+  <v-card :title="props.name" hover class="mt-8" elevation="2">
+    <template v-slot:subtitle>
+      <v-icon icon="mdi-timer" size="20"></v-icon>
+      {{ props.pauseDuration + "s" }}
+    </template>
+
+    <template v-slot:append>
+      <v-btn icon="mdi-pencil" color="primary" size="small" class="mr-2"></v-btn>
+      <v-btn icon="mdi-delete" size="small"></v-btn>
+    </template>
+  </v-card>
 </template>
 
 <script setup lang="ts">
