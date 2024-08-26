cb597b0fa327e253e738088daa7f89a00a8c4daf
Tien Vuong Vu <vutienvu@cvut.cz>
2024-08-26 16:35:22 +0200

    [refactor]: Change to ActionModal

--- src/app/components/Exercise.vue
+++ src/app/components/Exercise.vue
@@ -6,15 +6,56 @@
     </template>
 
     <template v-slot:append>
-      <v-btn icon="mdi-pencil" color="primary" size="small" class="mr-2" @click="(e: Event) => handleEdit(e)"></v-btn>
-      <v-btn icon="mdi-delete" size="small" @click="(e: Event) => handleRemove(e)"></v-btn>
+      <v-btn icon="mdi-pencil" color="primary" size="small" class="mr-2" @click="(e: Event) => handleUpdateExercise(e)"></v-btn>
+      <v-btn icon="mdi-delete" size="small" @click="(e: Event) => handleRemoveExercise(e)"></v-btn>
     </template>
   </v-card>
+
+  <ActionModal v-model:open-modal="isUpdatingExercise">
+  <template v-slot:header>
+    <v-card-item prepend-icon="mdi-pencil">
+      <v-card-title>Update your exercise!</v-card-title>
+    </v-card-item>
+  </template>
+
+  <template v-slot:input>
+    <form @submit.prevent>
+      <v-text-field label="Exercise name" variant="underlined" class="px-6"></v-text-field>
+      <v-text-field label="Exercise pause duration" variant="underlined" class="px-6"></v-text-field>
+    </form>
+  </template>
+
+  <template v-slot:actionButton>
+    <v-btn class="ms-auto" variant="elevated" color="primary">
+      Update
+    </v-btn>
+  </template>
+</ActionModal>
+  <ActionModal v-model:open-modal="isRemovingExercise">
+    <template v-slot:header>
+      <v-card-item prepend-icon="mdi-delete">
+        <v-card-title>Delete your exercise!</v-card-title>
+      </v-card-item>
+    </template>
+
+    <template v-slot:input>
+      <v-card-text>
+        Are you sure you want to remove this exercise?
+      </v-card-text>
+    </template>
+
+    <template v-slot:actionButton>
+      <v-btn class="ms-auto" variant="elevated" color="red">
+        Delete
+      </v-btn>
+    </template>
+  </ActionModal>
 </template>
 
 <script setup lang="ts">
 import {ref} from 'vue'
 import {deleteExercise, ExerciseResponse} from '../api/ExerciseAPI.ts'
+import ActionModal from './ActionModal.vue'
 
 interface Props {
   exercise: ExerciseResponse
@@ -24,27 +65,31 @@ const { exercise } = defineProps<Props>();
 
 const exercises = defineModel<ExerciseResponse[]>('exercises', { required: true });
 
-const isOpenEdit = ref<boolean>(false);
+const isUpdatingExercise = ref<boolean>(false);
+const isRemovingExercise = ref<boolean>(false);
 
 const handleOpen = () => {
   console.log("Open exercise!");
 }
 
-const handleEdit = (e: Event) => {
+const handleUpdateExercise = (e: Event) => {
   e.stopPropagation();
-  isOpenEdit.value = true;
+  isUpdatingExercise.value = true;
 }
 
-const handleRemove = (e: Event) => {
-  e.stopPropagation();
+const handleRemoveExercise = (event: Event) => {
+  event.stopPropagation();
+  isRemovingExercise.value = true;
 
-  deleteExercise(props.exerciseId)
-      .then(() => {
-        exercises.value = exercises.value.filter(e => e.exerciseId !== props.exerciseId);
-      })
-      .catch(error => {
-        console.log(error);
-      });
+  // e.stopPropagation();
+  //
+  // deleteExercise(exercise.exerciseId)
+  //     .then(() => {
+  //       exercises.value = exercises.value.filter(e => e.exerciseId !== exercise.exerciseId);
+  //     })
+  //     .catch(error => {
+  //       console.log(error);
+  //     });
 }
 
 </script>
