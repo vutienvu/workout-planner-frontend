930a7afa0b979d4f6fc27f46909e8f82b00907dc
Tien Vuong Vu <vutienvu@cvut.cz>
2024-08-27 20:56:23 +0200

    [feat]: Implement base api class

--- src/app/api/BaseAPI.ts
+++ src/app/api/BaseAPI.ts
@@ -1,8 +1,116 @@
-import axios from 'axios';
+import axios, {AxiosResponse as Response, AxiosRequestConfig as RequestConfig} from 'axios';
 
-const axiosInstance = axios.create({
-    baseURL: "http://localhost:5249",
-    timeout: 10000
-});
+class BaseAPI {
+    axiosInstance = axios.create({
+        baseURL: import.meta.env.VITE_API_URL,
+        timeout: 10000
+    });
 
-export default axiosInstance;
+    _reallyMakeRequest(requestConfig: RequestConfig | undefined) : Promise<Response> {
+        return new Promise((resolve, reject) => {
+            if (requestConfig === undefined) {
+                reject();
+                return;
+            }
+
+            this.axiosInstance.request(requestConfig)
+                .then(resolve)
+                .catch(reject);
+        });
+    }
+
+    _getPath(url: (string | number)[]): string {
+        return url.join('/');
+    }
+
+    getAll<T>(url: (string | number)[]): Promise<T[]> {
+        return new Promise((resolve, reject) => {
+            const requestConfig = {
+                url: this._getPath(url),
+                method: 'GET'
+            } as RequestConfig;
+
+            this._reallyMakeRequest(requestConfig)
+                .then(response => {
+                    resolve(response.data);
+                })
+                .catch(error => {
+                    reject(error);
+                });
+        });
+    }
+
+    get<T>(url: (string | number)[]): Promise<T> {
+        return new Promise((resolve, reject) => {
+            const requestConfig = {
+                url: this._getPath(url),
+                method: 'GET'
+            } as RequestConfig;
+
+            this._reallyMakeRequest(requestConfig)
+                .then(response => {
+                    resolve(response.data);
+                })
+                .catch(error => {
+                    reject(error);
+                });
+        })
+    }
+
+    post<T>(url: (string | number)[], data: any, contentType = 'application/json'): Promise<T> {
+        return new Promise((resolve, reject) => {
+            const requestConfig = {
+                url: this._getPath(url),
+                method: 'POST',
+                data: data,
+                headers: {
+                    'content-type': contentType
+                }
+            } as RequestConfig;
+
+           this._reallyMakeRequest(requestConfig)
+               .then(response => {
+                   resolve(response.data);
+               })
+               .catch(error => {
+                   reject(error);
+               });
+        });
+    }
+
+    delete(url: (string | number)[]): Promise<Response> {
+        return new Promise((resolve, reject) => {
+            const requestConfig = {
+                url: this._getPath(url),
+                method: 'DELETE'
+            } as RequestConfig;
+
+            this._reallyMakeRequest(requestConfig)
+                .then(resolve)
+                .catch(reject);
+        });
+    }
+
+    put<T>(url: (string | number)[], data: any, contentType = 'application/json'): Promise<T> {
+        return new Promise((resolve, reject) => {
+            const requestConfig = {
+                url: this._getPath(url),
+                method: 'PUT',
+                data: data,
+                headers: {
+                    'content-type': contentType
+                }
+            } as RequestConfig;
+
+            this._reallyMakeRequest(requestConfig)
+                .then(response => {
+                    resolve(response.data);
+                })
+                .catch(error => {
+                    reject(error);
+                });
+        });
+    }
+}
+
+export default BaseAPI;
